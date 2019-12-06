5ca266907c9d885774a804e039a3c197d9d5c8e8
Ambro17 <ambro17.1@gmail.com>
2019-12-05 23:44:38 -0300

    Add Listado UI mockup. Missing behaviour yet

--- FrbaOfertas/ListadoEstadistico/Form1.Designer.cs
+++ FrbaOfertas/ListadoEstadistico/Form1.Designer.cs
@@ -28,11 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(327, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(764, 33);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seleccionar Semestre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 209);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1035, 620);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // listar
+            // 
+            this.listar.Location = new System.Drawing.Point(670, 128);
+            this.listar.Margin = new System.Windows.Forms.Padding(6);
+            this.listar.Name = "listar";
+            this.listar.Size = new System.Drawing.Size(421, 57);
+            this.listar.TabIndex = 10;
+            this.listar.Text = "Proveedores con Mayor Facturación";
+            this.listar.UseVisualStyleBackColor = true;
+            this.listar.Click += new System.EventHandler(this.listar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(412, 54);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Proveedores con Mejores Descuentos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 865);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button listar;
+        private System.Windows.Forms.Button button2;
+
     }
 }
--- FrbaOfertas/ListadoEstadistico/Form1.cs
+++ FrbaOfertas/ListadoEstadistico/Form1.cs
@@ -16,5 +16,35 @@ namespace FrbaOfertas.ListadoEstadistico
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
